0bbb360e206e1d05970b69f891e02228f5c5b93a
lucasmoraist <luksmnt1101@gmail.com>
2025-09-25 18:37:57 -0300

    first commit

---
+++ src/main/java/com/codelab/wp_config_server/WpConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelab/wp_config_server/WpConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
